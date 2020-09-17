d8592a37a61c29ec783270a4c7d8fa7ee689c440
qdotdash <qdotdash@gmail.com>
2020-09-18 04:49:48 +0530

    luck

--- index.js
+++ index.js
@@ -55,22 +55,22 @@ client.on('ready', () => {
     var d = new Date();
     var n = d.getDay()
     var timestring = d.getHours()+":"+d.getMinutes();
-    if(timestring=="4:46"){
+    if(timestring=="8:30"){
       channel.send(timetablearray[n-1][0].message);
     }
-    else if(timestring=="4:48"){
+    else if(timestring=="9:30"){
       channel.send(timetablearray[n-1][1].message);
     }
-    else if(timestring=="4:50"){  
+    else if(timestring=="10:30"){  
       channel.send(timetablearray[n-1][2].message);
     }
-    else if(timestring=="4:53"){
+    else if(timestring=="11:30"){
       channel.send(timetablearray[n-1][3].message);
     }
-    else if(timestring=="4:51"){
+    else if(timestring=="12:30"){
       channel.send(timetablearray[n-1][4].message);
     }
-    else if(timestring=="4:28"){
+    else if(timestring=="14:0"){
       channel.send(timetablearray[n-1][5].message);
     }
 //     const user = <client>.users.cache.get('<id>');
