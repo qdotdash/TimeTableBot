02ecedb71a36748970b90b92b8c153474175052c
qdotdash <qdotdash@gmail.com>
2020-09-18 04:10:28 +0530

    final

--- index.js
+++ index.js
@@ -51,26 +51,26 @@ client.on('ready', () => {
 
 
   cron.schedule("* * * * *", function() {
-    const channel = client.channels.cache.get('756049115267465256');
+    const channel = client.channels.cache.get('756283093173797035');
     var d = new Date();
     var n = d.getDay()
     var timestring = d.getHours()+":"+d.getMinutes();
-    if(timestring=="8:30"){
+    if(timestring=="4:10"){
       channel.send(timetablearray[n-1][0].message);
     }
-    else if(timestring=="9:30"){
+    else if(timestring=="4:11"){
       channel.send(timetablearray[n-1][1].message);
     }
-    else if(timestring=="10:30"){
+    else if(timestring=="4:12"){
       channel.send(timetablearray[n-1][2].message);
     }
-    else if(timestring=="11:30"){
+    else if(timestring=="4:13"){
       channel.send(timetablearray[n-1][3].message);
     }
-    else if(timestring=="12:30"){
+    else if(timestring=="4:14"){
       channel.send(timetablearray[n-1][4].message);
     }
-    else if(timestring=="14:0"){
+    else if(timestring=="4:15"){
       channel.send(timetablearray[n-1][5].message);
     }
 //     const user = <client>.users.cache.get('<id>');
