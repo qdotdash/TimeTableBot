21d4fe003533b121c7a883dc6f235acef2fdbeae
qdotdash <qdotdash@gmail.com>
2020-09-18 04:42:55 +0530

    hi

--- index.js
+++ index.js
@@ -55,19 +55,19 @@ client.on('ready', () => {
     var d = new Date();
     var n = d.getDay()
     var timestring = d.getHours()+":"+d.getMinutes();
-    if(timestring=="4:32"){
+    if(timestring=="4:46"){
       channel.send(timetablearray[n-1][0].message);
     }
-    else if(timestring=="4:33"){
+    else if(timestring=="4:48"){
       channel.send(timetablearray[n-1][1].message);
     }
-    else if(timestring=="4:25"){  
+    else if(timestring=="4:50"){  
       channel.send(timetablearray[n-1][2].message);
     }
-    else if(timestring=="4:26"){
+    else if(timestring=="4:53"){
       channel.send(timetablearray[n-1][3].message);
     }
-    else if(timestring=="4:27"){
+    else if(timestring=="4:51"){
       channel.send(timetablearray[n-1][4].message);
     }
     else if(timestring=="4:28"){
