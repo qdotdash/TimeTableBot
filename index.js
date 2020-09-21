bd69ead83aef931253b971c39324910b0048bc91
thepedmu <32743228+thepedmu@users.noreply.github.com>
2020-09-21 13:40:16 +0530

    Added Welcome Message to Index.js

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 const Discord = require('discord.js');
+const Canvas = require('canvas');
 
 const client = new Discord.Client();
+
+
+client.on('message', message => {
+	if (message.content === '!joinfake') {
+		client.emit('guildMemberAdd', message.member);
+	}
+});
+
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./welcomeeva.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	
+	ctx.font = ctx.fillText(canvas, `${member.displayName}`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer());
+
+	channel.send(attachment);
+});
+
+
+
 
 const cron = require("node-cron");
     const express = require("express");
