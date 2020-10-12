54d9803b8ee401368065035ca61d026eb02c7744
qdotdash <qdotdash@gmail.com>
2020-10-12 12:58:23 +0530

    Cleaned up and timetable send function to group channels

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const Canvas = require('canvas');
 
 const client = new Discord.Client();
 
+//////////////////////////////////////////////////////////////////////////////////////TESTING WELCOME BANNER FUNCTIONALITY
 
 client.on('message', message => {
 	if (message.content === '!joinfake') {
 		client.emit('guildMemberAdd', message.member);
 	}
 });
+
+//////////////////////////////////////////////////////////////////////////////////////WELCOME BANNER FOR NEW MEMBERS
 
 client.on('guildMemberAdd', async member => {
 	const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
@@ -45,122 +48,128 @@ client.on('guildMemberAdd', async member => {
 	channel.send(attachment);
 });
 
-
-
+//////////////////////////////////////////////////////////////////////////////////////BASIC INITIALISATIONS
 
 const cron = require("node-cron");
-    const express = require("express");
-    const fs = require("fs");
+const express = require("express");
+const fs = require("fs");
+const { send } = require('process');
 
-    app = express();
-    //honors in seperate channel
-    //clear up subjects
-    //time duration of the period
-    //using seperate server or dm or channel permission learn
-    //7hour day check
-    //test other cron hours
-    //adding birthdays
+app = express();
 
 
-    var cns = {message: "Cryptography and Network Security(CNS) by George Mathew : https://meet.google.com/iyc-stko-ycn", subject: "CNS - George Mathew"};
-    var ml = {message: "Machine Learning(ML) by Ezu : https://meet.google.com/dni-bzrr-war", subject: "ML - Ezu"};
-    var cg = {message: "Computer Graphics(CG) by Jayasree : https://meet.google.com/odp-ayya-trc", subject: "CG - Jayasree"};
-    var csa = {message: "Computer System Architecture(CSA) by Mumthas : http://meet.google.com/aan-gzyr-xin", subject: "CSA - Mumthas"};
-    var dc = {message: "Distributed Computing(DC) by Bisna : http://meet.google.com/shm-xotx-ydd", subject: "DC - Bisna"};
-    var dip = {message: "Digital Image Processing(DIP) by Jayasree : https://meet.google.com/rcm-cdom-hwh", subject: "DIP - Jayasree"};
-    var lab = {message: "Compiler Design Lab(CDL) by Mumthas and Kala : http://meet.google.com/zvg-pobm-piz", subject: "CD Lab - Mumthas and Kala"};
-    var pp = {message: "Programming Paradigms(PP) : No meet links as of my knowledge", subject: "PP - Valsaraj"};
-    var project = {message : "Project hour under Valsaraj", subject: "Project - Valsaraj"};
-    var seminar = {message : "Seminar hour under Ajay James", subject: "Seminar - Ajay James"};
-    var honors = {message : "Honors : Digital Image Processing(DIP) by Jayasree : https://meet.google.com/rcm-cdom-hwh", subject: "Honors(DIP) - Jayasree"};
-
-    var timetablearray = [[ pp,   ml, project,  seminar, seminar, honors ],
-                          [ cns,  cg,      ml,       pp,     lab,    lab ],
-                          [ csa,  cns,     cg,  seminar, seminar,   lab  ],
-                          [ dc,   csa,     cg,      ml,     cns,  honors ],
-                          [ cg,   dc,     csa,      pp,     dc,   honors ]];
-
-     
-  var weekday = ["Sunday",  "Monday",  "Tuesday",  "Wednesday", "Thursday",  "Friday",  "Saturday"];
+//////////////////////////////////////////////////////////////////////////////////////CLIENT LOGIN
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
+//////////////////////////////////////////////////////////////////////////////////////TIMETABLE VARIABLE INITIALISATIONS
+
+var cns = {message: "Cryptography and Network Security(CNS) by George Mathew : https://meet.google.com/iyc-stko-ycn", subject: "CNS - George Mathew"};
+var ml = {message: "Machine Learning(ML) by Ezu : https://meet.google.com/dni-bzrr-war", subject: "ML - Ezu"};
+var cg = {message: "Computer Graphics(CG) by Jayasree : https://meet.google.com/odp-ayya-trc", subject: "CG - Jayasree"};
+var csa = {message: "Computer System Architecture(CSA) by Mumthas : http://meet.google.com/aan-gzyr-xin", subject: "CSA - Mumthas"};
+var dc = {message: "Distributed Computing(DC) by Bisna : http://meet.google.com/shm-xotx-ydd", subject: "DC - Bisna"};
+var dip = {message: "Digital Image Processing(DIP) by Jayasree : https://meet.google.com/rcm-cdom-hwh", subject: "DIP - Jayasree"};
+var lab = {message: "Compiler Design Lab(CDL) by Mumthas and Kala : http://meet.google.com/zvg-pobm-piz", subject: "CD Lab - Mumthas and Kala"};
+var pp = {message: "Programming Paradigms(PP) : No meet links as of my knowledge", subject: "PP - Valsaraj"};
+var project = {message : "Project hour under Valsaraj", subject: "Project - Valsaraj"};
+var seminar = {message : "Seminar hour under Ajay James", subject: "Seminar - Ajay James"};
+var honors = {message : "Honors : Digital Image Processing(DIP) by Jayasree : https://meet.google.com/rcm-cdom-hwh", subject: "Honors(DIP) - Jayasree"};
+
+var timetablearray = [[ pp,   ml, project,  seminar, seminar, honors ],
+                      [ cns,  cg,      ml,       pp,     lab,    lab ],
+                      [ csa,  cns,     cg,  seminar, seminar,   lab  ],
+                      [ dc,   csa,     cg,      ml,     cns,  honors ],
+                      [ cg,   dc,     csa,      pp,     dc,   honors ]];
+
+     
+var weekday = ["Sunday",  "Monday",  "Tuesday",  "Wednesday", "Thursday",  "Friday",  "Saturday"];
+
+
+//////////////////////////////////////////////////////////////////////////////////////CHANNELS TO SEND TIMETABLES
+
+const channel = client.channels.cache.get('756451422069063711');
+const channelgallery = client.channels.cache.get('752834723973300247');
+
+//////////////////////////////////////////////////////////////////////////////////////FUNCTION TO SEND TIMETABLE MESSAGE TO CHANNEL
+
+  function sendTimeTableMessage(m){
+      channel.send(m);
+      channelgallery.send(m)
+  }
+
+//////////////////////////////////////////////////////////////////////////////////////FUNCTION TO CONVERT TO MINUTES
+
+  function tominutes(h, m){
+    return h*60 + m;
+ }
+
+//////////////////////////////////////////////////////////////////////////////////////FUNCTION RETURNING THE HELP COMMAND STRING
+
+ function help(){
+   var commandstring = "\n**go to lab** or **go to csa** - display the subject meetlink\n\n**current class** - display current class\n\n**timetable today** - display today's timetable\n\n**timetable help** - display the commands available";
+   return commandstring;
+ }
+
+
+//////////////////////////////////////////////////////////////////////////////////////CRON SCHEDULE FUNCTION
 
   cron.schedule("* * * * *", function() {
-    const channel = client.channels.cache.get('756451422069063711');
-    const channelgallery = client.channels.cache.get('752834723973300247');
-
     var currentTime = new Date();
-
     var ISTOffset = 330;   // IST offset UTC +5:30 
-
-var indiaTime = new Date(currentTime.getTime() + (ISTOffset)*60000);
-
-    var n = indiaTime.getDay()
+    var indiaTime = new Date(currentTime.getTime() + (ISTOffset)*60000);
+    var n = indiaTime.getDay();
     var timestring = indiaTime.getHours()+":"+indiaTime.getMinutes();
-    //use a single current class var to assign the current class and show it when the current class is called
     
     if(n!=6&&n!=0){
       if(indiaTime.getDay()==5)
       {
-	       if(timestring=="8:00"){
-		channel.send(weekday[indiaTime.getDay()] + ", Hour 1: " + timetablearray[n-1][0].message);
-	      }
-	      else if(timestring=="8:55"){
-		channel.send("Hour 2: " + timetablearray[n-1][1].message);
-	      }
-	      else if(timestring=="09:50"){  
-		channel.send("Hour 3: " + timetablearray[n-1][2].message); 
-	      }
-	      else if(timestring=="10:45"){
-		channel.send("Hour 4: " + timetablearray[n-1][3].message);
-	      }
-	      else if(timestring=="11:40"){
-		channel.send("Hour 5: " + timetablearray[n-1][4].message);
-	      }
-	      else if(timestring=="14:0"){
-		channel.send("Hour 6: " + timetablearray[n-1][5].message);
-	      }
+        if(timestring=="8:00"){
+          sendTimeTableMessage(weekday[indiaTime.getDay()] + ", Hour 1: " + timetablearray[n-1][0].message);
+        }
+        else if(timestring=="8:55"){
+          sendTimeTableMessage("Hour 2: " + timetablearray[n-1][1].message);
+        }
+        else if(timestring=="09:50"){  
+          sendTimeTableMessage("Hour 3: " + timetablearray[n-1][2].message);
+        }
+        else if(timestring=="10:45"){
+          sendTimeTableMessage("Hour 4: " + timetablearray[n-1][3].message);
+        }
+        else if(timestring=="11:40"){
+          sendTimeTableMessage("Hour 5: " + timetablearray[n-1][4].message);
+        }
+        else if(timestring=="14:0"){
+          sendTimeTableMessage("Hour 6: " + timetablearray[n-1][5].message);
+        }
       }
-      else
+      else                        
       {
 	      if(timestring=="8:30"){
-		channel.send(weekday[indiaTime.getDay()] + ", Hour 1: " + timetablearray[n-1][0].message);
+		      sendTimeTableMessage(weekday[indiaTime.getDay()] + ", Hour 1: " + timetablearray[n-1][0].message);
 	      }
 	      else if(timestring=="9:30"){
-		channel.send("Hour 2: " + timetablearray[n-1][1].message);
+		      sendTimeTableMessage("Hour 2: " + timetablearray[n-1][1].message);
 	      }
 	      else if(timestring=="10:30"){  
-		channel.send("Hour 3: " + timetablearray[n-1][2].message); 
+		      sendTimeTableMessage("Hour 3: " + timetablearray[n-1][2].message); 
 	      }
 	      else if(timestring=="11:30"){
-		channel.send("Hour 4: " + timetablearray[n-1][3].message);
+		      sendTimeTableMessage("Hour 4: " + timetablearray[n-1][3].message);
 	      }
 	      else if(timestring=="12:30"){
-		channel.send("Hour 5: " + timetablearray[n-1][4].message);
+		      sendTimeTableMessage("Hour 5: " + timetablearray[n-1][4].message);
 	      }
 	      else if(timestring=="14:0"){
-		channel.send("Hour 6: " + timetablearray[n-1][5].message);
+	  	    sendTimeTableMessage("Hour 6: " + timetablearray[n-1][5].message);
 	      }
       }
     }
-//     const user = <client>.users.cache.get('<id>');
-// user.send('<content>'); send dm 752835857081303052
   });
 
-  function tominutes(h, m){
-     return h*60 + m;
-  }
-
-  function help(){
-    var commandstring = "\n**go to lab** or **go to csa** - display the subject meetlink\n\n**current class** - display current class\n\n**timetable today** - display today's timetable\n\n**timetable help** - display the commands available";
-    return commandstring;
-  }
-
-
-  //lower case this case insensitive
+//////////////////////////////////////////////////////////////////////////////////////SENDING REPLIES TO MESSAGES
   client.on('message', msg => {
     var messagestring = msg.content.toLowerCase();
     if (messagestring === 'go to cns') {
@@ -240,6 +249,7 @@ var indiaTime = new Date(currentTime.getTime() + (ISTOffset)*60000);
     }
   });
 
+//////////////////////////////////////////////////////////////////////////////////////AUTHENTICATION
 
 
 client.login(process.env.TOKEN);
